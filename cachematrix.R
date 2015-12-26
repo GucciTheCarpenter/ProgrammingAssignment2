e6ff27f3be6355db38e7b1296feef878906bf0ff
GucciTheCarpenter <stancutr@gmail.com>
2015-12-26 13:33:28 +0100

    submit: cache R file

--- cachematrix.R
+++ cachematrix.R
@@ -6,7 +6,13 @@
 ## set/get matrix; set/get matrix inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    # cache passed matrix
+    cache_M <<- x
+    getinv <- function(m) {
+        solve(m)
+    }
+    # cache matrix inverse
+    cache_I <<- getinv(x)
 }
 
 
@@ -14,5 +20,18 @@ makeCacheMatrix <- function(x = matrix()) {
 ## already calculated
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    # check cache and return accordingly
+    if(all(x == cache_M)) {
+        return(cache_I)
+    } else {
+        solve(x)
+    }
 }
+
+
+## testing
+x <- matrix(1:4, 2, 2)
+y <- matrix(1:4, 2, 2, byrow = TRUE)
+makeCacheMatrix(x)
+cacheSolve(x)
+cacheSolve(y)
